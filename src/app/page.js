2742c619ea0cec888a327115d8655613d96b0329
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2024-07-18 16:44:41 +0300

    added some styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import {authorization} from "./page.module.css";
+import {authorization, formLabel, formControl} from "./page.module.css";
 import React from 'react'
 import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap'
 import laptop from "../../public/svg/laptop.svg"
@@ -10,26 +10,29 @@ export default function Login() {
       <Row className="vh-100 d-flex justify-content-center align-items-center">
         <Col md={8} lg={6} xs={12}>
           
-          <Card className="shadow" className={authorization}>
+          <Card className={authorization}>
             <Card.Body>
               <div className="mb-3 mt-4 text-center">
 			  <Image src={laptop}  alt="Логотип"/>
-                <h5 className="fw-bold mb-2">Кафедра <br></br> “Кібернетика та штучний інтелект”</h5>
+                <h2 className="fw-bold">Кафедра <br></br> “Кібернетика та штучний інтелект”</h2>
                 <h1>Вхід</h1>
-				<p className="mb-5">Увійти як адміністратор</p>
+				<p>Увійти як адміністратор</p>
                 <Form className="mb-3">
                   <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label className="text-center">Введіть логін</Form.Label>
-                    <Form.Control type="text" placeholder="" />
+                    <Form.Label className={formLabel}>Введіть логін</Form.Label>
+                    <Form.Control className={formControl} type="text" placeholder="" />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Введіть пароль</Form.Label>
-                    <Form.Control type="password" placeholder="" />
+                    <Form.Label className={formLabel}>Введіть пароль</Form.Label>
+                    <Form.Control className={formControl} type="password" placeholder="" />
+
+
+                    
                   </Form.Group>
                   
                   <div className="d-grid">
-                    <Button variant="primary" type="submit">
+                    <Button variant="secondary" type="submit">
                       Увійти
                     </Button>
                   </div>
