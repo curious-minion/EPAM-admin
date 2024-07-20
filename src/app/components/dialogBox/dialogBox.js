b8ccb986a514ed2d498ef08dcd3f131c4b859338
courios-minion <okarskaolya@gmail.com>
2024-07-20 17:07:07 +0200

    added modal to the page

--- src/app/components/dialogBox/dialogBox.js
+++ src/app/components/dialogBox/dialogBox.js
@@ -1,27 +1,56 @@
+import Raact,  {useCallback, useEffect, useRef} from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function DialogBox() {
-  return (
-    <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
+function DialogBox({show, onHide}) {
+	const modalWrapperRef = useRef();
+
+	const backDropHandler = (e) => {
+		if (modalWrapperRef.current && modalWrapperRef.current.contains(e.target)){
+            onHide();
+        }
+    };
+
+  useEffect(() => {
+        setTimeout(() => {
+            window.addEventListener('click', backDropHandler);
+        })
+    }, [show])
+
+  useEffect(() => {
+		return () => {
+			window.removeEventListener('click', backDropHandler);
+		}
+    }, [show]);
+
+
+	return (
+
+     <Modal
+      show={show}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+			centered
+			ref={modalWrapperRef}
     >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Видалити публікацію?</Modal.Title>
-        </Modal.Header>
+
+
+
+			 <Modal.Dialog>
+
+          <Modal.Title id="contained-modal-title-vcenter">Видалити публікацію?</Modal.Title>
 
         <Modal.Body>
           <p>Ви дісно хочете видалити публікацію “Етика та відповідальність у розробці штучного інтелекту: аналіз і рекомендації”? Публікація буде видалена назавжди.</p>
         </Modal.Body>
 
         <Modal.Footer>
-          <Button variant="secondary">Відхилити</Button>
+          <Button variant="secondary" onClick={onHide}>Відхилити</Button>
           <Button variant="danger">Видалити</Button>
         </Modal.Footer>
-      </Modal.Dialog>
-    </div>
+</Modal.Dialog>
+</Modal>
+
   );
 }
 
